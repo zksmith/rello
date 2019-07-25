7d9df8e3036e369beeb1cf02889cc1d6b251e2e8
zksmith <zahiersmith96@gmail.com>
2019-07-25 16:35:19 -0400

    removed redux dev tools added redux logger to dev env

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
-// import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 const middlewares = [];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
