aba5ce2bc34ccf2dd26f0caa668a8fae506c5d24
zksmith <zahiersmith96@gmail.com>
2020-02-17 16:01:09 -0500

    typo

--- src/components/task-container/Task.js
+++ src/components/task-container/Task.js
@@ -24,7 +24,7 @@ const Task = ({
 
   const handleTaskUpdate = e => {
     e.preventDefault();
-    if (window.confirm(`Are you sure you wish to update "${subject}?" task`))
+    if (window.confirm(`Are you sure you wish to update "${subject}" task?`))
       updateTask(id, subjectCopy, descriptionCopy);
     setModalOpen(false);
   };
@@ -39,14 +39,22 @@ const Task = ({
             {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
-            <p style={{width: '90%'}}>{subject}</p>
+            <p style={{ width: '90%' }}>{subject}</p>
             <button
               className='task-edit'
               onClick={() => {
                 setModalOpen(true);
               }}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" viewBox="0 0 24 24"><path d="M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z"/></svg>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='20'
+                height='20'
+                fill='white'
+                viewBox='0 0 24 24'
+              >
+                <path d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z' />
+              </svg>
             </button>
           </div>
         )}
@@ -101,7 +109,7 @@ const Task = ({
                 e.preventDefault();
                 if (
                   window.confirm(
-                    `Are you sure you wish to delete "${subject}?" task`
+                    `Are you sure you wish to delete "${subject}" task?`
                   )
                 )
                   deleteTask(collectionId, id);
