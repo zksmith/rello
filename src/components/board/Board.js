cd678a6e687c3aa2ce15826d8c4ff5cf1d40be98
zksmith <zahiersmith96@gmail.com>
2020-06-23 23:23:48 -0400

    github link

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { connect } from "react-redux";
-import { DragDropContext } from "react-beautiful-dnd";
+import React from 'react';
+import { connect } from 'react-redux';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import {
   moveTask,
-  moveCollection
-} from "../../redux/actions/collectionActions";
+  moveCollection,
+} from '../../redux/actions/collectionActions';
 
-import CollectionsContainer from "../collection-container/CollectionContainer";
+import CollectionsContainer from '../collection-container/CollectionContainer';
 
-import BoardHeader from "./BoardHeader";
-import "./Board.scss";
+import BoardHeader from './BoardHeader';
+import './Board.scss';
 
 const Board = ({ totalTasks, moveTask, moveCollection, removeTaskId }) => {
-  const onDragEnd = result => {
+  const onDragEnd = (result) => {
     const { destination, source, draggableId, type } = result;
     if (!destination) return;
     if (
@@ -23,13 +23,13 @@ const Board = ({ totalTasks, moveTask, moveCollection, removeTaskId }) => {
       return;
     }
 
-    if (type === "task") {
+    if (type === 'task') {
       moveTask({
         collectionId: destination.droppableId,
         prevCollectionId: source.droppableId,
         taskId: draggableId,
         sourceIndex: source.index,
-        destinationIndex: destination.index
+        destinationIndex: destination.index,
       });
     } else {
       moveCollection(source.index, destination.index, draggableId);
@@ -37,19 +37,30 @@ const Board = ({ totalTasks, moveTask, moveCollection, removeTaskId }) => {
   };
 
   return (
-    <main className="board">
+    <main className='board'>
       <BoardHeader />
       <DragDropContext onDragEnd={onDragEnd}>
         <CollectionsContainer />
       </DragDropContext>
+      <a className='github-link' href='https://github.com/zksmith/rello'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='#fff'
+        >
+          <path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z'></path>
+        </svg>
+      </a>
     </main>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  moveTask: args => dispatch(moveTask(args)),
+const mapDispatchToProps = (dispatch) => ({
+  moveTask: (args) => dispatch(moveTask(args)),
   moveCollection: (sourceIndex, destinationIndex, collectionId) =>
-    dispatch(moveCollection(sourceIndex, destinationIndex, collectionId))
+    dispatch(moveCollection(sourceIndex, destinationIndex, collectionId)),
 });
 
 export default connect(null, mapDispatchToProps)(Board);
